9b1acf4ae84e92279f0d205dbb7e0a6809765294
Winson Tan <winsont89@gmail.com>
2019-04-09 15:06:57 +1000

    AWESOME WORKING ROBOT

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,6 +2,7 @@ require_relative 'robot'
 require_relative 'view'
 require 'pry'
 
+# Initiate Controller class
 class Controller
   def initialize(robot)
     @robot = robot
@@ -9,35 +10,12 @@ class Controller
   end
 
   def rename
-    name = @view.rename
+    @robot.name = @view.rename
   end
 
-
-
   def start_robot
-    # Receive input
-    # Validate input
-    # Update robot
-    # Update robot
-    begin
-      input = @view.start_robot
-      coordinates = input[6..-1].split(',') #Coordinates placements and direction, split into an array delimited by ','
-    rescue
-      retry
-    end
-    x_input = coordinates[0]
-    y_input = coordinates[1]
-    direction_input = coordinates[2]
-
-    valid_xy = ['0', '1', '2', '3', '4']
-    valid_direction = ['NORTH', 'SOUTH', 'EAST', 'WEST']
-    #Checks if input is OK
-    if input[0..5] == 'PLACE ' && (valid_xy.include? x_input) && (valid_xy.include? y_input) && (valid_direction.include? direction_input)
-      #Update robot position
-      @robot.position[:x] = x_input.to_i
-      @robot.position[:y] = y_input.to_i
-      @robot.position[:direction] = direction_input
-    # Move Robot
+    input = @view.start_robot(@robot)
+    if validate_placement(input)
       move_robot
     else
       start_robot
@@ -45,14 +23,12 @@ class Controller
   end
 
   def move_robot
-    selected_move = @view.move(@robot)
-
-    # Check if valid move, robot doesn't move if move is invalid
-    # if @robot.move(selected_move)
+    selected_move = @view.move
     while selected_move != 'REPORT'
-      ### CHECK FOR VALID PLACE MOVE
-      @robot.move(selected_move)
-      selected_move = @view.move(@robot)
+      if validate_placement(selected_move) != true
+        @robot.move(selected_move)
+        selected_move = @view.move
+      end
     end
     report_status
   end
@@ -61,7 +37,27 @@ class Controller
     @view.status(@robot)
   end
 
-  def validate(input)
-    coordinates = input[6..-1].split(',') #Coordinates placements and direction, split into an array delimited by ','
+  private
+
+  def validate_placement(input)
+    begin
+      coordinates = input[6..-1].split(',')
+      x_input = coordinates[0].to_i
+      y_input = coordinates[1].to_i
+      direction_input = coordinates[2]
+    rescue
+      return false
+    end
+    if input[0..5] == 'PLACE ' && (Robot::VALID_XY_COORDS.include? x_input) && (Robot::VALID_XY_COORDS.include? y_input) && (Robot::VALID_DIRECTIONS.include? direction_input)
+      update_position(x_input, y_input, direction_input)
+    else
+      false
+    end
+  end
+
+  def update_position(x_input, y_input, direction_input)
+    @robot.position[:x] = x_input
+    @robot.position[:y] = y_input
+    @robot.position[:direction] = direction_input
   end
 end
--- lib/robot.rb
+++ lib/robot.rb
@@ -1,45 +1,65 @@
+# Initiate Robot class
 class Robot
   attr_accessor :name, :position
+  VALID_XY_COORDS = [0, 1, 2, 3, 4].freeze
+  VALID_DIRECTIONS = ['NORTH', 'SOUTH', 'EAST', 'WEST'].freeze
 
   def initialize
-    @name = name || 'AwesomeO v1.0'
+    @name = 'AwesomeO v1.0'
     @position = { x: 0, y: 0, direction: 'NORTH' } # TODO: for thing
   end
 
   def move(selected_move)
     if selected_move == 'MOVE'
-      case @position[:direction]
-      when 'NORTH'
-        @position[:y] += 1
-      when 'SOUTH'
-        @position[:y] -= 1
-      when 'EAST'
-        @position[:x] += 1
-      when 'WEST'
-        @position[:x] -= 1
-      end
+      move_it
     elsif selected_move == 'LEFT'
-      case @position[:direction]
-      when 'NORTH'
-        @position[:direction] = 'WEST'
-      when 'SOUTH'
-        @position[:direction] = 'EAST'
-      when 'EAST'
-        @position[:direction] = 'NORTH'
-      when 'WEST'
-        @position[:direction] = 'SOUTH'
-      end
+      turn_left
     elsif selected_move == 'RIGHT'
-      case @position[:direction]
-      when 'NORTH'
-        @position[:direction] = 'EAST'
-      when 'SOUTH'
-        @position[:direction] = 'WEST'
-      when 'EAST'
-        @position[:direction] = 'SOUTH'
-      when 'WEST'
-        @position[:direction] = 'NORTH'
-      end
+      turn_right
+    end
+  end
+
+  private
+
+  def move_it
+    max_xy_coord = VALID_XY_COORDS[-1]
+    min_xy_coord = VALID_XY_COORDS[0]
+
+    case @position[:direction]
+    when 'NORTH'
+      @position[:y] == max_xy_coord ? @position[:y] : @position[:y] += 1
+    when 'SOUTH'
+      @position[:y] == min_xy_coord ? @position[:y] : @position[:y] -= 1
+    when 'EAST'
+      @position[:x] == max_xy_coord ? @position[:x] : @position[:x] += 1
+    when 'WEST'
+      @position[:x] == min_xy_coord ? @position[:x] : @position[:x] -= 1
+    end
+  end
+
+  def turn_left
+    case @position[:direction]
+    when 'NORTH'
+      @position[:direction] = 'WEST'
+    when 'SOUTH'
+      @position[:direction] = 'EAST'
+    when 'EAST'
+      @position[:direction] = 'NORTH'
+    when 'WEST'
+      @position[:direction] = 'SOUTH'
+    end
+  end
+
+  def turn_right
+    case @position[:direction]
+    when 'NORTH'
+      @position[:direction] = 'EAST'
+    when 'SOUTH'
+      @position[:direction] = 'WEST'
+    when 'EAST'
+      @position[:direction] = 'SOUTH'
+    when 'WEST'
+      @position[:direction] = 'NORTH'
     end
   end
 end
